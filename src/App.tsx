1c7ed301cb4faac5522a238b1121f53b3164c119
lasbe <metoo0339@naver.com>
2023-01-03 22:41:31 +0900

    Add: immutable, falsyTruthy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import { Immutable } from "./pages/immutable";
 import { Parent } from "./pages/renderTest/Parent";
+import { FalsyTruthy } from "./pages/falsyTruthy";
 
 function App() {
-  return <Parent />;
+  // return <Parent />;
+  return <FalsyTruthy />;
 }
 
 export default App;
---
+++ src/pages/immutable/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+// export const Immutable = () => {
+// const person = {
+//     name: "jang",
+//     age: 26,
+// };
+
+// return (
+//     <div>
+//         <span>{person.name}</span>
+//         <span>{person.age}</span>
+//         <button
+//         onClick={() => {
+//             person.age = 27;
+//             console.log(person.age);
+//                     // 콘솔에는 27이 찍히지만 상태를 감지할 수 없어 리렌더링은 일어나지 않는다.
+//         }}
+//         >
+//         Change Age
+//         </button>
+//     </div>
+// );
+// };
+
+export const Immutable = () => {
+  let person = 26;
+
+  console.log(person);
+  return (
+    <div>
+      {person}
+      <button
+        onClick={() => {
+          person = person + 1;
+        }}
+      ></button>
+    </div>
+  );
+};
