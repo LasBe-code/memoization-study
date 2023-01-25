59189b591faddd07ea1bdc513660ee71298fcc46
LAPTOP-RUPAEUDS\nuri <jsw0339@nuriflex.co.kr>
2023-01-25 11:14:10 +0900

    Style: 폴더 명 변경

--- src/pages/ConditionalRendering/index.tsx
+++ src/pages/ConditionalRendering/index.tsx
@@ -1,22 +1,46 @@
 import { useEffect, useState } from 'react';
 
-type StateType = {
-  person: { name: string };
+// 3초 후 return 하는 비동기 함수
+const promiseFun = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return 'JANG';
+};
+const Component = ({ name }: { name: string }) => {
+  return <button>{name}</button>;
+};
+
+const Loading = () => {
+  return <div>Loading...</div>;
 };
 
 export const ConditionalRendering = () => {
-  const [state, setState] = useState<StateType>();
+  const [state, setState] = useState<{ name: string }>();
 
   useEffect(() => {
-    setTimeout(() => {
-      const newData = { person: { name: 'kim' } };
-      setState(newData);
-    }, 1000);
+    promiseFun().then((response) => {
+      setState({ name: response });
+    });
   }, []);
 
-  return <>{state && <Component person={state.person} />}</>;
-};
+  // const renderWithIf = () => {
+  //   if(state) return <Component name={state.name} />
+  // }
 
-const Component = ({ person }: StateType) => {
-  return <button>{person.name}</button>;
+  // return renderWithIf();
+
+  // return state && <Component name={state.name} />
+
+  // return isModalOpen && <Modal />
+  // return stateA && stateB && stateC && <Component stateA={stateA} stateB={stateB} stateC={stateC} />
+
+  // const renderWithIfElse = () => {
+  //   if (state) {
+  //     return <Component name={state.name} />;
+  //   } else {
+  //     return <Loading />;
+  //   }
+  // }
+
+  // return renderWithIfElse()
+  return state ? <Component name={state.name} /> : <Loading />;
 };
